03c770ddaa8852c7b45f9ad22c10a9521489a384
yacoubhrm <114953698+yacoubhrm@users.noreply.github.com>
2024-05-13 10:46:17 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -34,4 +34,40 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 
+		#include <stdio.h>
+#include "./../include/calculator.h"
+
+int main(int argc, char *argv[]){
+	if (argc == 4)
+	{
+	char* op = argv[1];
+	char* a = argv[2];
+	char* b = argv[3];
+	double r = 0;
+	if (strcmp(op, "add") == 0) {
+		r = _add(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "sub") == 0) {
+		r = _sub(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "carre") == 0) {
+		r = _carre(atof(a));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "mul") == 0) {
+		r = _mul(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "div") == 0) {
+		r = _div(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else {printf("Erreur de parametres");}
+	}
+	else {printf("Erreur de parametres");}
+	return 0;
+}
+
 		
